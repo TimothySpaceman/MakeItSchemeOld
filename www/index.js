bf05a4be6b9f61a132a5db336e20b46bd9ff2257
TimothySpaceman <simetriumproject@gmail.com>
2022-10-13 22:41:28 +0300

    ПОВБИРАВ КРАПКИ З КОМАМИ НА СВОЮ ГОЛОВУ

--- www/index.js
+++ www/index.js
@@ -1,9 +1,21 @@
-const mainArea = document.querySelector("#mainArea");
-const lineNumbers = document.querySelector("#lineNumbers");
-const textEditor = document.querySelector("#textEditor");
-let text = textEditor.value;
-let tree = Array(1);
-let lines = Array(1);
+const mainArea = document.querySelector("#mainArea")
+const lineNumbers = document.querySelector("#lineNumbers")
+const textEditor = document.querySelector("#textEditor")
+let text = textEditor.value
+let tree = []
+let lines = []
+
+//| 0-Searching Condition | 1-Searching Yes | 2-Packing Yes | 3-Searching No | 4-Packing No | 5-Success |
+
+// Enum
+const statuses = {
+    SEARCHING_CONDITION: 0,
+    SEARCHING_YES: 1,
+    PACKING_YES: 2,
+    SEARCHING_NO: 3,
+    PACKING_NO: 4,
+    SUCCESS: 5
+}
 
 const types = {
     "Початок": "start",
@@ -15,180 +27,202 @@ const types = {
     "Так:": "yes",
     "Ні:": "no",
     "...": "end",
-    "Порожньо": "nothingness"
+    "Порожньо": "nothingness",
+
+    includes: function(fields) {
+        fields.forEach(el => {
+            if (this[el]) return this[el]
+        })
+
+        return this["Порожньо"]
+    }
 }
 
 function init(){
-    tree[0] = "none";
-    lines[0] = "none";
+    tree[0] = "none"
+    lines[0] = "none"
 }
 
-init();
+init()
 
 function updateEditor(){
     //debugger
-    text = textEditor.value;
-    lineNumbers.innerHTML ="";
+    text = textEditor.value
+    lineNumbers.innerHTML =""
 
-    let mainAreaHeight = ((textEditor.clientHeight-5)/23.75).toFixed(0);
-    for(let i=1;i<=mainAreaHeight;i++){
-        lineNumbers.innerHTML += i+"<br>";
+    let mainAreaHeight = ((textEditor.clientHeight-5)/23.75).toFixed(0)
+
+    for (let i=1;i<=mainAreaHeight;i++) {
+        lineNumbers.innerHTML += i+"<br>"
     }
 
-    textEditor.style.height = "1px";
-    textEditor.style.height = (textEditor.scrollHeight)+"px";
+    textEditor.style.height = "1px"
+    textEditor.style.height = (textEditor.scrollHeight)+"px"
 
-    //console.log(text);
+    //console.log(text)
 }
 
 function getLineType(line){
-    return line.split(/ /)[0];
+    return line.split(/ /)[0]
 }
 
 function getLineData(line){
-    let lineParts = line.split(/ /).slice(1);
-    return lineParts.join(" ");
+    let lineParts = line.split(/ /).slice(1)
+    return lineParts.join(" ")
 }
 
 function parseText(){
-    if(text != "" ) {
-        lines = text.split(/\r?\n/);
+    if (text != "" ) {
+        lines = text.split(/\r?\n/)
     }
 }
 
 function growTree(){
-    let lastElement = 0;
+    let lastElement = 0
     lines.forEach((line, index) => {
-        if(line != "" && (types[getLineType(line)] ?? false)){
+        if (line != "" && (types[getLineType(line)] ?? false)) {
             tree[lastElement] = {
                 type: types[getLineType(line)],
                 line: index,
                 text: getLineData(line),
                 actions: "none"
-            };
-            lastElement++;
+            }
+
+            lastElement++
         }
     })
 }
 
-function processConditions(index){
-    let i = index;
-    let condIndex = index;
-    const checkFinish = tree[i].type != types["Кінець"];
-    const checkProcessed = tree[i].actions != "none";
-    let status = 0; //| 0-Searching Condition | 1-Searching Yes | 2-Packing Yes | 3-Searching No | 4-Packing No | 5-Success |
+function processConditions(index, parent){
+    let i = index
+    let condIndex = index
+    const checkFinish = tree.type != types["Кінець"]
+    const checkProcessed = tree.actions != "none"
+    let status = statuses.SEARCHING_CONDITION; //| 0-Searching Condition | 1-Searching Yes | 2-Packing Yes | 3-Searching No | 4-Packing No | 5-Success |
+    let dotLevel = 0
 
     //Searching condition
-    if(status == 0){
-        while(tree[i].type != types["Умова"] && checkFinish){
-            i++;
-            if(i>=tree.length || !checkFinish){
-                return console.log("Error: Condition Not Found");
+
+    if (status == statuses.SEARCHING_CONDITION) {
+        while (tree[i].type != types["Умова"] && checkFinish) {
+            i++
+
+            if (i>=tree.length || !checkFinish) {
+                return console.log("Error: Condition Not Found")
             }
         }
-        if(!checkProcessed){
-            condIndex = i;
-            status = 1;
-        }
-        else {
-            processConditions(i+1);
-            return;
+
+        if (!checkProcessed) {
+            condIndex = i
+            status = statuses.SEARCHING_YES
+        } else {
+            //Почему ты считаешь что не сработает?
+            processConditions(i+1, tree[i])
+            return
         }
     }
 
     //Searching YES
-    if(status == 1){
-        while(tree[i].type != types["Так:"] && checkFinish){
-            i++;
-            if(i >= tree.length || !checkFinish){
-                return console.log("Error: Branch TRUE not found! Condition line: " + (tree[condIndex].line+1));
+    if (status == statuses.SEARCHING_YES) {
+        while (tree[i].type != types["Так:"] && checkFinish) {
+            i++
+
+            if (i >= tree.length || !checkFinish) {
+                return console.log("Error: Branch TRUE not found! Condition line: " + (tree[condIndex].line+1))
             }
         }
-        tree[condIndex].actions = {};
-        tree[condIndex].actions.yes = [];
-        //tree[condIndex].actions.yes[0] = new Object({type: types["Порожньо"]});
-        tree.splice(i, 1);
-        status = 2;
+
+        tree[condIndex].actions = {
+            yes: []
+        }
+        
+        tree.splice(i, 1)
+        status = statuses.PACKING_YES
+        dotLevel++
     }
 
-    console.log(tree[condIndex].actions.yes);
+    
     //Packing YES
-    if(status == 2){
-        while(tree[i].type != types["..."] && checkFinish){
-            if(tree[i].type == types["Умова"]){
-                processConditions(i);
-                i++;
-            }
-            else{
-                tree[condIndex].actions.yes.push(tree[i]);
-                tree.splice(i, 1);
+    if (status == statuses.PACKING_YES) {
+        while (checkFinish) {
+            if (tree[i].type == types.includes("Так:", "Ні:")) {
+                dotLevel++
+            } else if (tree[i].type == types["..."]) {
+                if (dotLevel == 1) {
+                    status = statuses.SEARCHING_NO
+                    dotLevel--
+                    parent.actions.yes.push(tree.splice(i, 1)[0])
+                    break
+                }
+
+                dotLevel--
             }
 
-            if(i >= tree.length || !checkFinish){
-                return console.log("Error: Branch TRUE not completed correctly! Condition line: " + (tree[condIndex].line+1));
+            parent.actions.yes.push(tree.splice(i, 1)[0])
+
+            if (i >= tree.length || !checkFinish) {
+                return console.log("Error: Branch TRUE not completed correctly! Condition line: " + (tree[condIndex].line+1))
             }
         }
-        if(tree[i].type == types["..."]){
-            tree[condIndex].actions.yes.push(tree[i]);
-            tree.splice(i, 1);
-        }
-        status = 3;
     }
 
     //Searching NO
-    if(status == 3){
-        while(tree[i].type != types["Ні:"] && checkFinish){
-            i++;
-            if(i >= tree.length || !checkFinish){
-                return console.log("Error: Branch FALSE not found! Condition line: " + (tree[condIndex].line+1));
+    if (status == statuses.SEARCHING_NO) {
+        while (tree[i].type != types["Ні:"] && checkFinish) {
+            i++
+            if (i >= tree.length || !checkFinish) {
+                return console.log("Error: Branch FALSE not found! Condition line: " + (tree[condIndex].line+1))
             }
         }
-        tree[condIndex].actions.no = [];
-        tree.splice(i, 1);
-        status = 4;
+
+        tree[condIndex].actions.no = []
+        
+        tree.splice(i, 1)
+        status = statuses.PACKING_NO
+        dotLevel++
     }
 
     //Packing NO
-    if(status == 4){
-        while(tree[i].type != types["..."] && checkFinish){
-            if(tree[i].type == types["Умова"]){
-                processConditions(i);
-                i++;
-            }
-            else{
-                tree[condIndex].actions.no.push(tree[i]);;
-                tree.splice(i, 1);
+    if (status == statuses.PACKING_NO) {
+        while (checkFinish) {
+            if (tree[i].type == types.includes("Так:", "Ні:")) {
+                dotLevel++
+            } else if (tree[i].type == types["..."]) {
+                if (dotLevel == 1) {
+                    status = statuses.SUCCESS 
+                    dotLevel--
+                    parent.actions.yes.push(tree.splice(i, 1)[0])
+                    break
+                }
+                dotLevel--
             }
 
-            if(i >= tree.length || !checkFinish){
-                return console.log("Error: Branch FALSE not completed correctly! Condition line: " + (tree[condIndex].line+1));
+            parent.actions.yes.push(tree.splice(i, 1)[0])
+            
+            if (i >= parent.length || !checkFinish) {
+                return console.log("Error: Branch FALSE not completed correctly! Condition line: " + (parent[condIndex].line+1))
             }
         }
-        if(tree[i].type == types["..."]){
-            tree[condIndex].actions.no.push(tree[i]);
-            tree.splice(i, 1);
-        }
-        status = 5;
     }
 
-    if(status == 5){
-        return console.log("Successfully processed condition in line: " + (tree[condIndex].line+1));;
+    if (status == statuses.SUCCESS) {
+        return console.log("Successfully processed condition in line: " + (parent[condIndex].line+1))
     }
 }
 
-window.addEventListener("keyup", updateEditor);
-window.addEventListener("keydown", updateEditor);
-window.addEventListener("mousemove", updateEditor);
+["keyup", "keydown", "mousemove"].forEach((el) => {
+    window.addEventListener(el, updateEditor)
+})
 
-function mainLoop(){
-    updateEditor();
+function mainLoop() {
+    updateEditor()
 
-    requestAnimationFrame(mainLoop);
+    requestAnimationFrame(mainLoop)
 }
 
-//mainLoop();
+//mainLoop()
 
-function goo(){
-    parseText();
-    growTree();
+function goo() {
+    parseText()
+    growTree()
 }
